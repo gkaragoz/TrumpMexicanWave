28cdb477210deb804282ddf08ab8f1be48f8dcd7
Derin Yarsuvat <derin@gram.gs>
2017-01-21 18:26:54 +0200

    Add the Character controllers as stubs

---
+++ Assets/Scripts/Characters/ViewerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Culture
+{
+	CAUCASIAN = 0,
+	MEXICAN = 1,
+	AFRICANAMERICAN = 2,
+	LIBERAL = 3,
+	MUSLIM = 4
+}
+
+public class ViewerController : MonoBehaviour
+{
+
+	[SerializeField]
+	private Culture _culture;
+	public Culture ActiveCulture
+	{
+		get
+		{
+			return _culture;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	public void StandUp()
+	{
+		LeanTween.moveY(
+			gameObject, 
+			transform.position.y + 0.5f, 
+			Random.Range(0.25f, 0.5f));
+	}
+
+	public void SitDown()
+	{
+		LeanTween.moveY(
+			gameObject,
+			transform.position.y - 0.5f,
+			Random.Range(0.25f, 0.5f));
+	}
+}
