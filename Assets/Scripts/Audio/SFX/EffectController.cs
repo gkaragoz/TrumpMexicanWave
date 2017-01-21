263b339b083c194cc70304dbca908eecdfea0d90
Derin Yarsuvat <derin@gram.gs>
2017-01-21 16:09:12 +0200

    Add Audio engine

---
+++ Assets/Scripts/Audio/SFX/EffectController.cs
@@ -0,0 +1,46 @@
+public class EffectController
+{
+	private readonly EffectPlayer _player;
+	private bool _isSfxOn;
+
+	public EffectController()
+	{
+		_player = EffectPlayer.NewPlayer();
+		_isSfxOn = true;
+	}
+
+	public void PlayEffect( Effect effect )
+	{
+		if ( _isSfxOn )
+		{
+			_player.PlayEffect( effect );
+		}
+	}
+
+	public void PlayEffectDistinctly( Effect effect )
+	{
+		if ( _isSfxOn )
+		{
+			_player.PlayEffectDistinctly( effect );
+		}
+	}
+
+	public void StopPlaying()
+	{
+		_player.Stop();
+	}
+
+	public void StopPlayingDistinctEffect( Effect effect )
+	{
+		_player.StopDistinctEffect( effect );
+	}
+
+	private void OnSfxSettingsChanged()
+	{
+		_isSfxOn = !_isSfxOn;
+	}
+
+	public void Destroy()
+	{
+	}
+}
