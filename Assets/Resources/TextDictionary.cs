d0be073b8e20a21775eca6ee05ec830280a64113
Derin Yarsuvat <derin@gram.gs>
2017-01-22 14:50:03 +0200

    Add text dictionary with newspaper responses

---
+++ Assets/Resources/TextDictionary.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class TextDictionary : MonoBehaviour
+{
+
+	public static Dictionary<Culture,string> CultureLines = 
+		new Dictionary<Culture, string>
+	{
+		{ Culture.MEXICAN, "Trump claims majority of Mexicans are rapists!" },
+		{ Culture.LIBERAL, "Trump claims media has Liberal bias!"},
+		{ Culture.WOMEN, "Trump grabs women by the pussy!"},
+		{ Culture.AFRICANAMERICAN, "Trump to African Americans: 'What do you have to lose?'" },
+		{ Culture.MUSLIM, "Trump to create a list of all Muslims in the U.S.!" },
+		{ Culture.CAUCASIAN, "Trump angers minority group!" } // Fallback
+	};
+
+
+	public static Dictionary<Culture,string> CultureSubLines = 
+		new Dictionary<Culture, string>
+	{
+		{ Culture.MEXICAN, "But some are good people!" },
+		{ Culture.LIBERAL, "Claims stream is lame!"},
+		{ Culture.WOMEN, "...They let him do ANYTHING!"},
+		{ Culture.AFRICANAMERICAN, "You have nothing; here, have some more!" },
+		{ Culture.MUSLIM, "Obama to be included!" },
+		{ Culture.CAUCASIAN, "Majority groups enthralled!" } // Fallback
+	};
+
+}
