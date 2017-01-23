8e2b4b46c6ad8fb42eca659ff57806b6ab294032
Gökhan Karagöz <annotto3@gmail.com>
2017-01-24 02:13:06 +0300

    Add particle effects like hearth and confetti

---
+++ Assets/Scripts/Particles/ParticleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleController : MonoBehaviour {
+
+    public GameObject[] ParticleEffects;
+    public static ParticleController Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    public void ParticleItRandomly(Vector3 position)
+    {
+        int random = Random.Range(0, ParticleEffects.Length);
+        Instantiate(ParticleEffects[random], position + (Vector3.up * 1.25f), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Particles/TimedObjectDestructor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedObjectDestructor : MonoBehaviour {
+
+    private float timeOut = 2.0f;
+    private bool detachChildren = false;
+
+    void Awake()
+    {
+        Invoke("DestroyNow", timeOut);
+    }
+
+    void DestroyNow()
+    {
+        if (detachChildren)
+            transform.DetachChildren();
+
+        DestroyObject(gameObject);
+    }
+}
